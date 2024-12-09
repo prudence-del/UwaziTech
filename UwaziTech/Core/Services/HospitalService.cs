86d41e0ace0cfa781d968f7c081c37ed01b13670
Allan Mutai <ALLAN.MUTAI@equitybank.co.ke>
2024-12-09 18:41:15 +0300

    Infra Config cleanup

--- UwaziTech/Core/Services/HospitalService.cs
+++ UwaziTech/Core/Services/HospitalService.cs
@@ -3,19 +3,41 @@ using UwaziTech.API.Model.Response;
 using UwaziTech.Core.Models;
 using UwaziTech.Core.Models.request;
 using UwaziTech.Core.Services.Interfaces;
+using UwaziTech.Infrastructure.Context;
 
 namespace UwaziTech.Core.Services;
 
 public class HospitalService : IHospitalService
 {
+    private readonly AppDbContext _appDbContext;
+    public HospitalService(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
     public async Task<ApiResponse<HospitalAdminDetails>> AddHospitalAdminAsync(HospitalAdminDetails request, CancellationToken token)
     {
-        return new ApiResponse<HospitalAdminDetails>
+        _appDbContext.HospitalAdminDetail.Add(request);
+        
+        var result = await _appDbContext.SaveChangesAsync(token) > 0;
+
+        if (!result)
         {
-            StatusCode = ResponseCode.OK,
-            StatusMessage = StatusMessage.PENDING_IMPLEMENTATION,
-            ResponseObject = request,
-        };
+            return new ApiResponse<HospitalAdminDetails>
+            {
+                StatusCode = ResponseCode.OK,
+                StatusMessage = StatusMessage.DB_ADD_SUCCESSFUL,
+                ResponseObject = request,
+            };
+        }
+        else
+        {
+            return new ApiResponse<HospitalAdminDetails>
+            {
+                StatusCode = ResponseCode.FAILED,
+                StatusMessage = StatusMessage.PENDING_IMPLEMENTATION,
+                ResponseObject = request,
+            };
+        }
     }
 
     public async Task<ApiResponse<HospitalDetails>> AddHospitalDetailsAsync(HospitalDetails request, CancellationToken token)
--- UwaziTech/Infrastructure/Context/AppDbContext.cs
+++ UwaziTech/Infrastructure/Context/AppDbContext.cs
@@ -10,17 +10,19 @@ namespace UwaziTech.Infrastructure.Context
 
 
         //Adding DBset properties for my tables
+        public DbSet<UserDetails> UserDetail { get; set; }
         public DbSet<AdminDetails> AdminDetail { get; set; }
-        public DbSet<InsuranceDetails> InsuranceDetail { get; set; }
         public DbSet<InvoiceDetails> InvoiceDetails { get; set; }
         
         public DbSet<HospitalDetails> HospitalDetail { get; set; }
+        public DbSet<InsuranceDetails> InsuranceDetail { get; set; }
         
         public DbSet<HospitalAdminDetails> HospitalAdminDetail { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<UserDetails>().Property(x => x.Reference);
             modelBuilder.Entity<InsuranceDetails>().HasKey(e => e.Id);
             modelBuilder.Entity<AdminDetails>().HasKey(e => e.Reference);
             modelBuilder.Entity<InvoiceDetails>().HasKey(e => e.Reference);
